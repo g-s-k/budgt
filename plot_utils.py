0c64ad25eff90acc2c432618dd39d95b7c86ef8e
George <george@georgekaplan.xyz>
2018-02-05 07:41:21 -0500

    Fixed cumsum problem

--- plot_utils.py
+++ plot_utils.py
@@ -6,26 +6,28 @@ def project_balances(n_days, accounts, transactions):
     days = np.arange(0, n_days) * dt.timedelta(days=1) + dt.date.today()
     trans_lst = []
     for trans in transactions:
-        trans_lst.append({"name": trans["name"], "source": trans["source"],
-                          "dest": trans["dest"]})
         if trans["frequency"] == "daily":
-            trans_lst[-1]["value"] = np.ones(days.shape)
+            is_tr_day = np.ones(days.shape)
         elif trans["frequency"] == "weekly":
-            trans_lst[-1]["value"] = np.array([day.weekday() == trans["day"] for day in days])
+            is_tr_day = np.array([day.weekday() == trans["day"] for day in days])
         elif trans["frequency"] == "monthly":
-            trans_lst[-1]["value"] = np.array([day.day == trans["day"] for day in days])
+            is_tr_day = np.array([day.day == trans["day"] for day in days])
         else:
-            trans_lst[-1]["value"] = np.array([day.timetuple().tm_yday == trans["day"] for day in days])
+            is_tr_day = np.array([day.timetuple().tm_yday == trans["day"] for day in days])
+        trans_lst.append({"name": trans["name"], "source": trans["source"],
+                          "dest": trans["dest"],
+                          "value": trans["amount"] * is_tr_day})
     for account in accounts:
         account = dict(account)
         account["init_balance"] = (-1)**(account["positive"] + 1) * \
             account["balance"] - account["holds"]
-        account["value"] = np.zeros(days.shape) + account["init_balance"]
+        account["value"] = np.zeros(days.shape)
         for trans in trans_lst:
             if trans["source"] == account["name"]:
                 account["value"] -= trans["value"]
             elif trans["dest"] == account["name"]:
                 account["value"] += trans["value"]
-        plt.plot(days, account["value"])
+        plt.plot(days, np.cumsum(account["value"]) + account["init_balance"], label=account["name"])
     # display the projections
+    plt.legend()
     plt.show()
