fef1697e5656106d863d2fab237abcd7579770e6
George <george@georgekaplan.xyz>
2018-02-05 23:41:16 -0500

    Added verbose messages

--- plot_utils.py
+++ plot_utils.py
@@ -11,7 +11,7 @@ def is_today(day, trans):
     cases.append(trans["frequency"] == "annual" and trans["day"] == day.timetuple().tm_yday)
     return any(case for case in cases)
 
-def project_balances(n_days, accounts, transactions):
+def project_balances(n_days, accounts, transactions, verbosity=0):
     # figure out initial state
     accts = {a["name"]: dict(a) for a in accounts}
     for a in accts.keys():
@@ -23,14 +23,29 @@ def project_balances(n_days, accounts, transactions):
             if is_today(date_vec[day], trans):
                 try:
                     if trans["source"] in accts and accts[trans["source"]]["positive"] and accts[trans["source"]]["value"][day] - trans["amount"] < accts[trans["source"]]["min_balance"]:
-                            raise ValueError
+                            raise ValueError("insufficient funds in account {0}".format(trans["source"]))
                     if trans["dest"] in accts and not accts[trans["dest"]]["positive"] and accts[trans["dest"]]["value"][day] + trans["amount"] > 0:
-                            raise ValueError
+                            raise ValueError("credit account {0} cannot have positive balance".format(trans["dest"]))
+                    in_acct = []
                     if trans["source"] in accts:
+                        in_acct.append("s")
                         accts[trans["source"]]["value"][day:] -= trans["amount"]
                     if trans["dest"] in accts:
+                        in_acct.append("d")
                         accts[trans["dest"]]["value"][day:] += trans["amount"]
-                except ValueError:
+                    if verbosity:
+                        fmt_str = "Transaction '{0}' "
+                        if "s" in in_acct and "d" in in_acct:
+                            fmt_str += "transferred ${1:.02f} from account '{2}' to account '{3}'"
+                        elif "s" in in_acct:
+                            fmt_str += "decreased account '{2}' balance by ${1:.02f}"
+                        elif "d" in in_acct:
+                            fmt_str += "increased account '{3}' balance by ${1:.02f}"
+                        fmt_str += " on day {4} of {5}"
+                        print(fmt_str.format(trans["name"], trans["amount"], trans["source"], trans["dest"], day, n_days))
+                except ValueError as ve:
+                    if verbosity > 1:
+                        print("Transaction '{0}' failed on day {1} of {2} because {3}".format(trans["name"], day, n_days, str(ve)))
                     pass
     # plot it
     for n, a in accts.items():
